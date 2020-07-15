f94503f11b74bc8e46f2785c1c0c1722ba192862
Kayo Timoteo Rossales <timoteokayo@gmail.com>
2020-07-14 22:05:05 -0400

    App para CI sem Keys

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,33 @@
 import 'react-native-gesture-handler';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import SplashScreen from 'react-native-splash-screen';
 
 import AppProvicer from './hooks';
 
 import Routes from './routes';
 
-const App: React.FC = () => (
-  <NavigationContainer>
-    <StatusBar barStyle="light-content" backgroundColor="#312e38" translucent />
-    <AppProvicer>
-      <View style={{ flex: 1, backgroundColor: '#312e38' }}>
-        <Routes />
-      </View>
-    </AppProvicer>
-  </NavigationContainer>
-);
+const App: React.FC = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
+  return (
+    <NavigationContainer>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="#312e38"
+        translucent
+      />
+      <AppProvicer>
+        <View style={{ flex: 1, backgroundColor: '#312e38' }}>
+          <Routes />
+        </View>
+      </AppProvicer>
+    </NavigationContainer>
+  );
+};
 
 export default App;
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.0.106:3333',
+  baseURL: 'http://64.227.9.109',
 });
 
 export default api;
